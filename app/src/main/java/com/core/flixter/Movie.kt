f3d135d3646257db8bbca2d0eeab4b6122eb2992
DeadS3ct <71579884+DeadS3ct@users.noreply.github.com>
2022-02-19 19:56:37 -0500

    Initial commit

---
+++ app/src/main/java/com/core/flixter/Movie.kt
@@ -0,0 +1,24 @@
+package com.core.flixter
+
+import org.json.JSONArray
+
+data class Movie (private val posterPath: String, val overview: String, val movieId: Int, val title: String){
+    val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+    companion object{
+        fun fromJsonArray(moviesJsonArray: JSONArray):List<Movie> {
+            val movies = mutableListOf<Movie>()
+            for (i in 0 until moviesJsonArray.length()){
+                val moviesJson = moviesJsonArray.getJSONObject(i)
+                movies.add(
+                    Movie(
+                        moviesJson.getString("poster_path"),
+                        moviesJson.getString("overview"),
+                        moviesJson.getInt("id"),
+                        moviesJson.getString("title")
+                    )
+                )
+            }
+            return movies
+        }
+    }
+}
